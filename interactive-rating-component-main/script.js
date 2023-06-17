98212da1748c224ee1c1abcf41e7b3d50f2195a9
meliodas-png <132607975+meliodas-png@users.noreply.github.com>
2023-06-17 22:34:02 +0100

    Added final javascript

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,11 +1,27 @@
 let textEl = document.getElementById("text")
-let rate = 4;
-function one(number) {
-    rate = number;
+let rate = "blur";
+const one = () => {
+     rate = 1;
+     return text.innerHTML = "You selected " + rate + " out of 5";
 }
-
-function submit() {
-    text.innerHTML = `You selected ${rate} out of 5`
+const two = () => {
+     rate = 2;
+     return text.innerHTML = "You selected " + rate + " out of 5";
+}
+const three = () => {
+     rate = 3;
+     return text.innerHTML = "You selected " + rate + " out of 5";
+}
+const four = () => {
+     rate = 4;
+     return text.innerHTML = "You selected " + rate + " out of 5";
+}
+const five = () => {
+     rate = 5;
+     return text.innerHTML = "You selected " + rate + " out of 5";
 }
 one()
-submit()
+two()
+three()
+four()
+five()
